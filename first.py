714101e1cc1f08855aea6ca0473fd78a6dea9724
rahulhetta <rahulhetta94@gmail.com>
2024-03-11 20:55:44 +0530

    array syntax

--- first.py
+++ first.py
@@ -1,55 +1,60 @@
-print("my name is rahul","i am 19 years old")
-#we can aALSO do calculations 
-print(34+34)
-print(34-23)
+# print("my name is rahul","i am 19 years old")
+# #we can aALSO do calculations 
+# print(34+34)
+# print(34-23)
 
-name="rahul"
-age = 19
-price = 34
-name2= name
-print(price)
+# name="rahul"
+# age = 19
+# price = 34
+# name2= name
+# print(price)
 
-print("my name is :", name2)
-print("my age is :", age)
+# print("my name is :", name2)
+# print("my age is :", age)
 
-#python can also find the type of value atored in variable
-print(type(name))
-print(type(age))
-print(type(price))
+# #python can also find the type of value atored in variable
+# print(type(name))
+# print(type(age))
+# print(type(price))
 
-#sum of two numbers
-a=4
-b=9
-sum=a+b
-print(sum)
+# #sum of two numbers
+# a=4
+# b=9
+# sum=a+b
+# print(sum)
 
-#comments in python
-#single line comments
+# #comments in python
+# #single line comments
 
-"""
-multi line
- comments
-"""
+# """
+# multi line
+#  comments
+# """
 
-# to comment multiple scentence ctrl+/
-# prinh '
-# ueihueghiueg
-# bueb'
+# # to comment multiple scentence ctrl+/
+# # prinh '
+# # ueihueghiueg
+# # bueb'
 
-#operators
-#arithematic operator
-#+,-,*,/,%,**
+# #operators
+# #arithematic operator
+# #+,-,*,/,%,**
 
-# relational/comparison operators
-a=50
-b=20
-#to check that a=b
-print(a==b) #false
-#a not equal yo b
-print(a!=b) #true
+# # relational/comparison operators
+# a=50
+# b=20
+# #to check that a=b
+# print(a==b) #false
+# #a not equal yo b
+# print(a!=b) #true
 
-#assignment operator
-num=10
-num=num+10
+# #assignment operator
+# num=10
+# num=num+10#
 
+
+# #array syntax
+
+a = [1,2,3,4]
+print(a)
 
